54a51f13702b3dbbee50d4c052af7463f7e15b5b
LAPTOP-10HM80O3\何鸿 <632681127@qq.com>
2025-07-14 13:53:39 +0800

    feat：添加初始化相关逻辑

--- vue/vue-source/src/init.js
+++ vue/vue-source/src/init.js
@@ -1,3 +1,4 @@
+import { compileToFunction } from "./compile";
 import { initState } from "./state";
 
 export function initMixin(Vue) {
@@ -7,5 +8,29 @@ export function initMixin(Vue) {
     vm.$options = options;
     // 初始化状态
     initState(vm);
+
+    if (options.el) {
+      vm.$mount(options.el);
+    }
+  };
+
+  Vue.prototype.$mount = function (el) {
+    const vm = this;
+    const opts = vm.$options;
+    el = document.querySelector(el);
+
+    if (!opts.render) {
+      let template;
+      if (opts.template) {
+        template = opts.template;
+      } else if (el) {
+        template = el.outerHTML;
+      }
+
+      if (template) {
+        const render = compileToFunction(template);
+        opts.render = render;
+      }
+    }
   };
 }
